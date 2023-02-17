9571cfeab4ab898fb2dc5cdac6e2a7286d247c59
younes1337 <raymondyounes2@gmail.com>
2023-02-17 16:31:41 +0100

    starting app on render.com

---
+++ src/server.js
@@ -0,0 +1,11 @@
+// this is the starting point for the app
+
+const http = require('http');
+const app = require('./app');
+ 
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+
+
+server.listen(PORT, () => console.log(`server started on port ${PORT}`));
