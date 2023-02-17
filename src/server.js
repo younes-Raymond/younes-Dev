4c5a2fb5ad331f9aad9dad8c24622a88f7267914
younes1337 <raymondyounes2@gmail.com>
2023-02-17 17:23:11 +0100

    adding a ejs package to node_module

--- src/server.js
+++ src/server.js
@@ -1,13 +1,60 @@
-// this is the starting point for the app
+// // this is the starting point for the app
 
-const http = require('http');
-const app = require('./app');
- 
-const PORT = process.env.PORT || 3000;
-const server = http.createServer(app);
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const ejs = require('ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('Home');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
 
 
 
-server.listen(PORT, () => console.log(`server started on port ${PORT}`));
-// hello
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require('http');
+// const expess = require('express');
+// const app = expess();
+
+// const app = require('./app');
+//  const ejs = require('ejs');
+
+
+
+
+// const PORT = process.env.PORT || 3000;
+// const server = http.createServer(app);
+
+
+
+// server.listen(PORT, () => console.log(`server started on port ${PORT}`));
+// // hello
+// ejs.renderFile('./views/Home.ejs', { data: 'Hello, world!' }, (err, str) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       console.log(str);
+//     }
+//   });
