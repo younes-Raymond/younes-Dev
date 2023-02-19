8b0c87875df5ad720b8d4f840d6e1ef816167629
younes1337 <raymondyounes2@gmail.com>
2023-02-19 17:50:50 +0100

    feat: Add contact form functionality and nodemailer integration
    - Update HTML code for the contact form to capture user inputs
    - Implement nodemailer to send form data to specified email address
    - Add styling to email content for improved user experience
    
    Link to website: https://younes-dev.onrender.com/

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
+const bodyParser = require('body-parser');
 const { Server } = require('http');
+const nodemailer = require('nodemailer');
 const path = require('path');
 app.use(express.json());
 app.set('views', path.join(__dirname,'views'));
@@ -14,6 +16,61 @@ app.get('/', (req, res) => {
 app.get('/Home', (req, res) => {
   res.render('Home');
 });
+
+// Parse incomig form data
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+// handle form submission 
+
+app.post('/submit-form', (req, res) => {
+  console.log(req.body)
+  const name = req.body.username;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  const subject = req.body.subject;
+  const message = req.body.message;
+
+  console.log(name)
+  console.log(phone)
+  console.log(email)
+  console.log(subject)
+  console.log(message)
+
+   // create reusable transporter object using the default SMTP transport
+   let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'raymondyounes2@gmail.com',
+      pass:"iqwvmellavcyatyl"
+    }
+  });
+
+  // setup email data 
+
+  // setup email data
+  let mailOptions = {
+    from: 'raymondyounes2@gmail.com',
+    to: 'youneshero436@gmail.com',
+    subject: 'New form submission',
+    html: `<p>Name: ${name}</p>
+           <p>Phone: ${phone}</p>
+           <p>Email: ${email}</p>
+           <p>Subject: ${subject}</p>
+           <p>Message: ${message}</p>`
+  };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        res.send('Error occurred, message not sent.');
+      } else {
+        console.log('Message sent: %s', info.messageId);
+        res.send('Message sent successfully!');
+      }
+    });
+  });
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`server listen at ${PORT}`))
