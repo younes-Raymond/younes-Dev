326881e98ccba192f1578c05383b1dd46fea1bc2
younes1337 <raymondyounes2@gmail.com>
2023-02-20 16:45:04 +0100

    feat: Add new features, update images, and improve email functionality
    This commit adds new features to the website, including modified feature boxes and a new testimonial section. It also updates the images used in the feature boxes and adds a thank-you email for clients who contact the website owner. Additionally, it improves the website's email functionality with the integration of Nodemailer and adds a form for contacting the website owner.
    
    The commit hash for this change is 717a74761ee500d0be8987bf037e63f4018a827b.

--- src/public/source/master.js
+++ src/public/source/master.js
@@ -155,7 +155,7 @@ randombackel.forEach(span => {
 let landingpage = document.querySelector(".landing-page");
 
 // get array of images 
-let imagearray  = ["/images/01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg","08.jpg","09.jpg","10.jpg"];
+let imagearray  = ["01.jpg","02.webp","03.jpg","04.jpg","05.jpeg","06.webp","07.jpeg","08.jpg","09.jpg","10.jpg"];
 
 // // random background option
 
